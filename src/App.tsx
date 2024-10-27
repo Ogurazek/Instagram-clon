612bfdf33e38868bf84765e673d24c63072b732b
Perez Elías <eg378556@gmail.com>
2024-10-26 23:51:27 -0300

    Día 3 Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { SideBar } from "./Components/Sidebar/sideBar"
-import { Home } from "./Components/Home/home"
-import { RightSideBar } from "./Components/RightSideBar/rightSideBar"
- 
+import { SideBar } from "./Components/Sidebar/sideBar";
+import { Home } from "./Components/Home/home";
+import { RightSideBar } from "./Components/RightSideBar/rightSideBar";
+import { useState } from "react";
+
 function App() {
+  const [showInfoCard, setShowInfoCard] = useState(false);
+  
+  const updateState = (state: boolean) => {
+    setShowInfoCard(state);
+  };
 
   return (
-      <>
-      <div style={{display: 'flex'}}>
-        <SideBar/>
-        <Home/>
-        <RightSideBar/>
+    <>
+      <div style={{ display: 'flex' }}>
+        {!showInfoCard && <SideBar />}
+        
+        <Home actualizarEstado={updateState} />
+        
+        {!showInfoCard && <RightSideBar />}
       </div>
-      </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
