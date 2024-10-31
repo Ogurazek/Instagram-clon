f365e763ce24d14016d1e6fa6c58af333fc3ebe6
Perez Elías <eg378556@gmail.com>
2024-10-31 18:46:31 -0300

    Sección de los Botones Finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@ import { SideBar } from "./Components/Sidebar/sideBar";
 import { Home } from "./Components/Home/home";
 import { RightSideBar } from "./Components/RightSideBar/rightSideBar";
 import { useState } from "react";
+import { SectionHome } from "./Components/SectionButton/sectionButton";
+
 
 function App() {
   const [showInfoCard, setShowInfoCard] = useState(false);
+  const [showHome, setShowHome] = useState(false);
   
   const updateState = (state: boolean) => {
     setShowInfoCard(state);
+  };
+  const updateStateHome = (state: boolean) => {
+    setShowHome(state);
   };
 
   return (
     <>
       <div style={{ display: 'flex' }}>
-        {!showInfoCard && <SideBar />}
+        {!showInfoCard && <SideBar actualizarEstadoHome={updateStateHome} />}
         
-        <Home actualizarEstado={updateState} />
+        {showHome ? <SectionHome /> : <Home actualizarEstado={updateState}/>}
         
-        {!showInfoCard && <RightSideBar />}
+        {!showInfoCard && !showHome && <RightSideBar />}
       </div>
     </>
   );
