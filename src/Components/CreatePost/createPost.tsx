ceb98a179844cf789dca5f44e82ddd2b90f9e45a
Perez Elías <eg378556@gmail.com>
2024-11-04 20:14:07 -0300

    Inputs terminados del CreatePost

--- src/Components/CreatePost/createPost.tsx
+++ src/Components/CreatePost/createPost.tsx
@@ -1,53 +1,102 @@
 import { useState } from 'react';
 import styles from './createPostStyles.module.css'
 
-export function CreatePost( {onClick}: {onClick:any;} ) {
+export function CreatePost({ onClick }: { onClick: any; }) {
     const [message, setMessage] = useState("");
+    const [urlImage, setUrlImage] = useState("");
+    const [namePost, setNamePost] = useState("");
+    const [imgProfile, setImgProfile] = useState("");
     const counter = message.length;
 
-    const messageRecu = (event: any) => {
-        setMessage(event.target.value)
+    const handleChangeImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUrlImage(event.target.value)
+    }
+    const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNamePost(event.target.value)
+    }
+    const handleChangeImageProfile = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setImgProfile(event.target.value)
     }
 
-    return(
+    const messageRecu = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(event.target.value)
+        console.log(message)
+
+    }
+
+
+    return (
         <>
-        <div className={styles.modal}>
-            <article className={styles.container_crp}>
-                <header className={styles.header_crp}>
-                <div className={styles.button_share_crp}>
-                <button >Compartir</button>
-                </div>
-                <span className={styles.span_crp}>
-                <strong>Crea una nueva publicación</strong>
-                </span>
-                <div className={styles.button_arrow_crp}>
-                <button onClick={onClick}>
-                    <img className={styles.nav_out_img} src="/Icons/Icon X.svg" alt="Salir" />
-                </button>
-                </div>
-                </header>
-                <article className={styles.article_container_form}>
-                <div className={styles.section_img}><img src="/Icons/Icon Loading.svg" alt="" /></div>
-                <section className={styles.section_form}>
-                    <div className={styles.section_span}>
-                        <img src="/Icons/Profile Image.jpg" alt="" />
-                        <strong>perezelias_</strong>
-                    </div>
-                    <div className={styles.section_form_textarea} >
-                    <textarea name="" id="" value={message} onChange={messageRecu} maxLength={250}></textarea>
-                    </div>
-                    <section className={styles.section_form_words}>
-                        <button><img src="/Icons/Icon Emoji.svg" alt="" /></button>
-                        <div><p>{counter}/250</p></div>
-                    </section>
-                    <div className={styles.container_input}>
-                        <span>Imagen(url)</span>
-                        <input type="text" placeholder='https://www.ejemplo.com' />
-                    </div>
-                </section>
+            <div className={styles.modal}>
+                <article className={styles.container_crp}>
+                    <header className={styles.header_crp}>
+                        <div className={styles.button_share_crp}>
+                            <button >Compartir</button>
+                        </div>
+                        <span className={styles.span_crp}>
+                            <strong>Crea una nueva publicación</strong>
+                        </span>
+                        <div className={styles.button_arrow_crp}>
+                            <button onClick={onClick}>
+                                <img className={styles.nav_out_img} src="/Icons/Icon X.svg" alt="Salir" />
+                            </button>
+                        </div>
+                    </header>
+                    <article className={styles.article_container_form}>
+                        <div className={styles.section_img}><img src={urlImage || "/Icons/Icon Loading.svg"} alt="" /></div>
+                        <section className={styles.section_form}>
+                            <form action="">
+                                <div className={styles.section_span}>
+                                    <img src={imgProfile || "/Icons/Icon Loading.svg"} alt="" />
+                                    <strong>{namePost || "Sin nombre"}</strong>
+                                </div>
+                                <div className={styles.section_form_textarea} >
+                                    <textarea
+                                        name=""
+                                        id=""
+                                        value={message}
+                                        onChange={messageRecu}
+                                        maxLength={250}></textarea>
+                                </div>
+                                <section className={styles.section_form_words}>
+                                    <button><img src="/Icons/Icon Emoji.svg" alt="" /></button>
+                                    <div><p>{counter}/250</p></div>
+                                </section>
+                                <div className={styles.container_input}>
+                                    <div>
+                                        <label>Imagen(url)</label>
+                                        <input
+                                            type="text"
+                                            placeholder='https://www.ejemplo.com'
+                                            value={urlImage}
+                                            onChange={handleChangeImage}
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>Nombre</label>
+                                        <input
+                                            type="text"
+                                            placeholder='perezelias_'
+                                            value={namePost}
+                                            onChange={handleChangeName}
+                                            maxLength={12}
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>Img Perfil</label>
+                                        <input
+                                            type="text"
+                                            placeholder='https://www.ejemplo.com'
+                                            value={imgProfile}
+                                            onChange={handleChangeImageProfile}
+                                        />
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </article>
                 </article>
-            </article>
-        </div>
+            </div>
         </>
     )
 }
--- src/Components/Sidebar/sideBar.tsx
+++ src/Components/Sidebar/sideBar.tsx
@@ -6,7 +6,7 @@ interface sideBarProps {
     actualizarEstadoModal: (NewState: boolean) => void;
 }
 
-export function SideBar ({ actualizarEstadoHome, actualizarEstadoModal }: sideBarProps) {
+export function SideBar({ actualizarEstadoHome, actualizarEstadoModal }: sideBarProps) {
 
     const handleClick = () => {
         actualizarEstadoHome(true)
@@ -18,42 +18,42 @@ export function SideBar ({ actualizarEstadoHome, actualizarEstadoModal }: sideBa
         actualizarEstadoModal(true)
         actualizarEstadoHome(false)
     }
-   
 
-    return(
+
+    return (
         <>
             <div className={styles.container_div_main}>
                 <header className={styles.header_logo}>
                     <a href=""><img src="/Icons/Instagram.svg" alt="Instagram" /></a>
                 </header>
-            <ButtonNavigation name="Inicio" icon="/Icons/Icon Home.svg" OnClick={handleClickComeBack} />
-            <ButtonNavigation name="Buscar" icon="/Icons/Icon Search.svg" OnClick={handleClick}/>
-            <ButtonNavigation name="Reels" icon="/Icons/Icons Reels.svg" OnClick={handleClick}/>
-            <ButtonNavigation name="Mensajes" icon="/Icons/Icon Messanger.svg" OnClick={handleClick}/>
-            <ButtonNavigation name="Notificaciones" icon="/Icons/Icon Favourite.svg" OnClick={handleClick}/>
-            <ButtonNavigation name="Crear" icon="/Icons/Icon more.svg" OnClick={handleClickModal}/>
-            <ButtonNavigation name="Perfil" icon="/Icons/Profile Image.jpg" OnClick={handleClick}/>
-            <section className={styles.section_menu}>
-                <ButtonNavigation name="Más" icon="/Icons/Icon Menu.svg" OnClick={handleClick}/>
-            </section>
+                <ButtonNavigation name="Inicio" icon="/Icons/Icon Home.svg" OnClick={handleClickComeBack} />
+                <ButtonNavigation name="Buscar" icon="/Icons/Icon Search.svg" OnClick={handleClick} />
+                <ButtonNavigation name="Reels" icon="/Icons/Icons Reels.svg" OnClick={handleClick} />
+                <ButtonNavigation name="Mensajes" icon="/Icons/Icon Messanger.svg" OnClick={handleClick} />
+                <ButtonNavigation name="Notificaciones" icon="/Icons/Icon Favourite.svg" OnClick={handleClick} />
+                <ButtonNavigation name="Crear" icon="/Icons/Icon more.svg" OnClick={handleClickModal} />
+                <ButtonNavigation name="Perfil" icon="/Icons/Profile Image.jpg" OnClick={handleClick} />
+                <section className={styles.section_menu}>
+                    <ButtonNavigation name="Más" icon="/Icons/Icon Menu.svg" OnClick={handleClick} />
+                </section>
             </div>
-            
+
         </>
     )
 }
 
 
-function ButtonNavigation({name, icon, OnClick}: {name: string, icon?: string, OnClick?: any,}) {
+function ButtonNavigation({ name, icon, OnClick }: { name: string, icon?: string, OnClick?: any, }) {
     const isProfile = name === "Perfil"; //Si el nombre es Perfil... Este tomara otra clase así le puedo agregar el radius
 
-    return(
+    return (
         <>
-         <nav className={styles.nav_container}>
-                    <button onClick={OnClick} className={isProfile ? styles.profile_button : ""}>
-                        <div className={styles.icon_img}>
+            <nav className={styles.nav_container}>
+                <button onClick={OnClick} className={isProfile ? styles.profile_button : ""}>
+                    <div className={styles.icon_img}>
                         <img src={icon} alt="Icon" />{name}
-                        </div>
-                    </button>
+                    </div>
+                </button>
             </nav>
         </>
     )
