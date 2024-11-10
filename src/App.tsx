1be13635f8ab19b16e6ad0008a7e5a4a38d619dd
Perez Elías <eg378556@gmail.com>
2024-11-10 19:50:44 -0300

    Arreglos en el tamaño

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Home } from "./Components/Home/home";
 import { RightSideBar } from "./Components/RightSideBar/rightSideBar";
 import { useState } from "react";
 import { SectionHome } from "./Components/SectionButton/sectionButton";
-
+import styles from './appStyles.module.css'
 
 function App() {
   const [showInfoCard, setShowInfoCard] = useState(false);
   const [showHome, setShowHome] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  
+
   const updateState = (state: boolean) => {
     setShowInfoCard(state);
   };
@@ -22,17 +22,21 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', overflow: 'hidden'}}>
-        {!showInfoCard && <SideBar actualizarEstadoHome={updateStateHome} actualizarEstadoModal={updateShowModal}/>}
-        
-        {showHome ? <SectionHome /> : 
-        <Home 
-        actualizarEstado={updateState} 
-        EstadoModal={showModal} 
-        actualizarEstadoModal={updateShowModal}
-        />}
-        
-        {!showInfoCard && !showHome && <RightSideBar />}
+      <div style={{ width: 'vw', height: 'vh' }}>
+        <div style={{ display: 'flex', overflow: 'hidden', justifyContent: 'space-between', }}>
+
+
+          {!showInfoCard && <SideBar actualizarEstadoHome={updateStateHome} actualizarEstadoModal={updateShowModal} />}
+
+          {showHome ? <SectionHome /> :
+            <Home
+              actualizarEstado={updateState}
+              EstadoModal={showModal}
+              actualizarEstadoModal={updateShowModal}
+            />}
+
+          {!showInfoCard && !showHome && <RightSideBar />}
+        </div>
       </div>
     </>
   );
